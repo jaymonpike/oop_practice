9dcd122d5a3dac32a0958ae83d0a206546fdb6e6
jaymonpike <jaymonpike@hotmail.com>
2024-11-19 18:28:18 -0700

    added @classmethod def get_all_vehicles(cls):

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ if __name__ == "__main__":
 
     #Create an instance of the Vehicle class
     black_car = Vehicle("Tesla", "Model 3", 2018)
+    red_car = Vehicle("Toyota", "Camry", 2023)
 
-    print(black_car)
+    all_vehicles = Vehicle.get_all_vehicles()
+    for vehicle in all_vehicles:
+        print(vehicle)
